8ea90cee2bc49c5a33fa7cc3fdff7f350e661d51
Sarah Raguin <sarahraguin@gmail.com>
2023-01-06 15:44:11 -0500

    First look to the project, moving images to assets and beginning work on fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,30 +1,30 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+    async function getPhotographers(url) {
+        const res = await fetch(url);
+        return await res.json();
     }
+        getPhotographers('http://127.0.0.1:8080/data/photographers.json')
+            .then(data => {
+                console.log(data);
+                let photographers = JSON.parse(data);
+
+                //photographers = [
+                //    {
+                //        "name": data.photographers.name,
+                //        "id": data.photographers.id,
+                //        "city": data.photographers.city,
+                //        "country": data.photographers.country,
+                //        "tagline": data.photographers.tagline,
+                //        "price": data.photographers.price,
+                //        "portrait": data.photographers.portrait
+                //    }
+                //]
+                // et bien retourner le tableau photographers seulement une fois récupéré
+                return ({
+                    photographers: [...photographers, ...photographers, ...photographers]})
+            })
+            .catch((err) => {
+                console.error('Error:', err);
+              });
 
     async function displayData(photographers) {
         const photographersSection = document.querySelector(".photographer_section");
