78b41bfa35b7a25386a655e72f9b829f77abedc1
Sarah Raguin <sarahraguin@gmail.com>
2023-01-13 13:56:17 -0500

    Console.log of personnal informations on each page of photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -3,6 +3,24 @@ function retrieveIdInParams() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
     let idInUrlParams = urlParams.get('id');
+    return idInUrlParams;
 }
 
-retrieveIdInParams();
+async function getPersonnalInformationsPhotographer() {
+    let photographersInformations = JSON.parse(photographersData);
+    let getIdFromUrl = retrieveIdInParams();
+    
+    for (let i = 0; i < photographersInformations.photographers.length; i ++) {
+            if (getIdFromUrl == photographersInformations.photographers[i].id) {
+                console.log(photographersInformations.photographers[i].name);
+                console.log(photographersInformations.photographers[i].city);
+                console.log(photographersInformations.photographers[i].country);
+                console.log(photographersInformations.photographers[i].tagline);
+                console.log(photographersInformations.photographers[i].price);
+            }
+    }
+    return ({
+        photographers: [...photographersInformations.photographers]})
+}
+
+getPersonnalInformationsPhotographer();
