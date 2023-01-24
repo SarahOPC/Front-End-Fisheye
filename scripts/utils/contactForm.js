386c8f287bfcf88d354403e1b29564daf83e498d
Sarah Raguin <sarahraguin@gmail.com>
2023-01-24 14:40:23 -0500

    Working on contact modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -7,3 +7,16 @@ function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
 }
+
+function insertName() {
+    const modal = document.querySelector("#contact_modal .modal header");
+    const firstH2 = document.querySelector("#contact_modal .modal header h2");
+    const name = document.querySelector("article a div h2").textContent;
+    const h2 = document.createElement( 'h2' );
+    h2.textContent = name;
+    firstH2.insertAdjacentHTML("afterBegin", h2);
+    modal.appendChild(h2);
+
+}
+
+insertName();
