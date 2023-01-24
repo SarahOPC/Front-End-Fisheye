567de08b865ad31b29790a27d3ea3068a3d9a3d1
Sarah Raguin <sarahraguin@gmail.com>
2023-01-24 15:36:11 -0500

    Still working on modal

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,22 +1,29 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+
+    const bodyHeader = document.querySelector("body header");
+    const bodyMain = document.querySelector("body main");
+    bodyHeader.setAttribute("class", "modalBlur");
+    bodyMain.setAttribute("class", "modalBlur");
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
     modal.style.display = "none";
+
+    const bodyHeader = document.querySelector("body header");
+    const bodyMain = document.querySelector("body main");
+    bodyHeader.setAttribute("class", "");
+    bodyMain.setAttribute("class", "");
 }
 
 function insertName() {
-    const modal = document.querySelector("#contact_modal .modal header");
     const firstH2 = document.querySelector("#contact_modal .modal header h2");
     const name = document.querySelector("article a div h2").textContent;
     const h2 = document.createElement( 'h2' );
     h2.textContent = name;
-    firstH2.insertAdjacentHTML("afterBegin", h2);
-    modal.appendChild(h2);
-
+    firstH2.insertAdjacentElement("afterend", h2);
 }
 
 insertName();
