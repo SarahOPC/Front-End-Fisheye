9c426be8848d4b3cd1c39dbb0954c4d008f61c1a
Sarah Raguin <sarahraguin@gmail.com>
2023-05-29 09:48:45 -0400

    Adding the possibility to fill the form of the modal for keyboard navigators

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 function displayModal() {
     const modal = document.getElementById("contact_modal");
+    modal.setAttribute("tabindex", "0");
 	modal.style.display = "block";
     insertName();
 
@@ -20,15 +21,24 @@ function closeModal() {
     bodyMain.removeAttribute("class");
 }
 
+function handleModalEscapeKey(event) {
+    if(event.key === 'Escape') {
+        closeModal();
+    }
+}
+
 function insertName() {
     const firstH2 = document.querySelector("#contact_modal .modal header h2");
     const name = document.querySelector("article a div h2").textContent;
     const h2 = document.createElement( 'h2' );
     h2.textContent = name;
     firstH2.insertAdjacentElement("afterend", h2);
+    const textInputUserFirstName = document.getElementById("userFirstName");
+    textInputUserFirstName.setAttribute("tabindex", "0");
+    textInputUserFirstName.focus(); // add a focus to be sure the input receives the focus when the modal is displayed
 }
 
-function retrieveDataFromForm() {
+function retrieveDataFromForm(event) {
     const userFirstName = document.querySelector(".modal-form #userFirstName").value;
     const userLastName = document.querySelector(".modal-form #userLastName").value;
     const userEmail = document.querySelector(".modal-form #userEmail").value;
@@ -37,6 +47,12 @@ function retrieveDataFromForm() {
     console.log(userLastName);
     console.log(userEmail);
     console.log(userMessage);
+    if(event === 13) {
+        console.log(userFirstName);
+        console.log(userLastName);
+        console.log(userEmail);
+        console.log(userMessage);
+    }
 }
 
 document.getElementById("contact_button_send").addEventListener("click", function(e){
